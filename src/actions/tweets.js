e1ec7fd64b3af70471a65acab876e26cf98debb4
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-11-02 19:29:04 +0300

    Handle Toogle Tweet action created

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,4 +1,7 @@
+import { saveLikeToggle } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOOGLE_TWEET = 'TOOGLE_TWEET'
 
 export function receiveTweets (tweets) {
     return {
@@ -6,3 +9,25 @@ export function receiveTweets (tweets) {
         tweets
     }
 }
+
+export function toggleTweet ({id, authedUser, hasLiked}) {
+    return {
+        type: TOOGLE_TWEET,
+        id,
+        authedUser,
+        hasLiked
+    }
+}
+
+export function handleToogleTweet (info) {
+    return (dispatch) => {
+        dispatch(toggleTweet(info))
+
+        return saveLikeToggle(info)
+            .catch((e) => {
+                console.warn('Error in handleToogleTweet: ', e)
+                dispatch(toggleTweet(info))
+                alert('There was an error liking the tweet. Try again.')
+            })
+    }
+} 
